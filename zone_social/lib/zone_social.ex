4a682dd3109ddf213eff9b158b6f61cbd75f0929
Jose Luis Escalante <38319711+escalante911@users.noreply.github.com>
2021-02-22 14:26:57 -0500

    Jose

--- zone_social/lib/zone_social.ex
+++ zone_social/lib/zone_social.ex
@@ -21,15 +21,27 @@ defmodule ZoneSocial do
     conn
   end
 
-  def insertOne(conn, coll, data) do
-    result = Mongo.insert_one!(conn, coll, data)
+  def insert_user(data) do
+    {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27027/zone-social", username: "root", password: "pass", auth_source: "admin")
+    result = Mongo.insert_one!(conn, "user", data)
     Map.put(data, :id, result.inserted_id)
   end
 
-  def consulta(conn, coll) do
-    result = Mongo.find(conn, coll, %{})
+  def consulta_user() do
+    {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27027/zone-social", username: "root", password: "pass", auth_source: "admin")
+    result = Mongo.find(conn, "user", %{})
     result |> Enum.to_list() |> IO.inspect
   end
+
+  def consulta_post() do
+    {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27027/zone-social", username: "root", password: "pass", auth_source: "admin")
+    result = Mongo.find(conn, "post", %{})
+    result |> Enum.to_list() |> IO.inspect
+  end
+
+  # def buscamigo(amigo) do
+
+  # end
 
 
 end
