a3dcad27fed995ff795b9435b6e5af1c95054986
Jose Luis Escalante <38319711+escalante911@users.noreply.github.com>
2021-02-22 16:23:33 -0500

    Jose

--- zone_social/lib/zone_social.ex
+++ zone_social/lib/zone_social.ex
@@ -21,15 +21,29 @@ defmodule ZoneSocial do
     conn
   end
 
-  def insert_user(data) do
+  def insert_user(nombre, usuario) do
+    data = %{name: nombre, user: usuario}
     {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27027/zone-social", username: "root", password: "pass", auth_source: "admin")
     result = Mongo.insert_one!(conn, "user", data)
     Map.put(data, :id, result.inserted_id)
   end
 
-  def consulta_user() do
+  def insert_post(mensaje, usuario) do
+    data = %{post: mensaje, user: usuario}
+    {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27027/zone-social", username: "root", password: "pass", auth_source: "admin")
+    result = Mongo.insert_one!(conn, "post", data)
+    Map.put(data, :id, result.inserted_id)
+  end
+
+  def consulta_users() do
     {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27027/zone-social", username: "root", password: "pass", auth_source: "admin")
     result = Mongo.find(conn, "user", %{})
+    result |> Enum.to_list() |> IO.inspect
+  end
+
+  def consulta_friend(name) do
+    {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27027/zone-social", username: "root", password: "pass", auth_source: "admin")
+    result = Mongo.find(conn, "user", %{"friend.name": name})
     result |> Enum.to_list() |> IO.inspect
   end
 
